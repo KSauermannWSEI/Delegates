e4cc72eef68bbd8ac8434dec1da68b20cf755a10
krzysztof.sauermann <krzysztof.sauermann@outlook.com>
2019-02-05 18:11:28 +0100

    Delegaty, anonimowe i lambda

--- Delegates/Program.cs
+++ Delegates/Program.cs
@@ -10,6 +10,53 @@ namespace Delegates
     {
         static void Main(string[] args)
         {
+            Action<string> cw = Console.WriteLine;
+            Action<int, int> x = Sum;
+            Func<int> cr = Console.Read;
+            cw("1");
+            cw += r => Console.WriteLine(r + r);
+            x += (a, b) => Console.WriteLine(a - b);
+            x += (a, b) =>
+            {
+                if (a > 1 && b != 0)
+                {
+                    Console.WriteLine(a % b);
+                }
+                if (a % b == 0)
+                {
+                    Console.WriteLine("Podzielana przez 2");
+                }
+            };
+            cr = () => Console.Read();
+
+            cw("2");
+            cw -= Console.WriteLine;
+            cw("3");
+            Console.WriteLine();
+            x(11, 3);
+            cr();
+
+            //List<Roman> romany = new List<Roman>();
+            //romany.Add(new Roman
+            //{
+            //    Name = "Prawdziwy Roman"
+            //});
+            //Roman roman = new Roman();
+            //roman.Name = "Romana";
+            //romany.Add(roman);
         }
+        class Roman
+        {
+            public string Name { get; set; }
+        }
+        static void Sum(int a, int b)
+        {
+            Console.WriteLine(a + b);
+        }
+        //static void writeNumber(string strNumber)
+        //{
+        //    //Jakaś logika
+        //    Console.WriteLine($"Podany string jest liczbą: {strNumber}");
+        //}
     }
 }
